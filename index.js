002c0f83fe48d11f0c2f7ed3d43f46e41f0d8874
Dima Keykin <vladdragonsun@gmail.com>
2019-03-27 16:30:47 +0200

    remove

--- index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { YellowBox } from 'react-native';
-
-
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-console.disableYellowBox = true;
-
-AppRegistry.registerComponent('carster', () => App);
